fef331ac313bb76ab74e950c8e5957910e45a664
Zwei <89715693+derzwei@users.noreply.github.com>
2021-09-13 13:58:21 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-# Sincronizar repositorios
-sudo pacman -Syy
+ln -sf /usr/share/zoneinfo/America/Argentina/Buenos_Aires /etc/localtime
+hwclock --systohc
+sed -i '177s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=la-latin1" >> /etc/vconsole.conf
+echo "arch" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 arch.localdomain arch" >> /etc/hosts
+echo root:password | chpasswd
 
-pacman -S efibootmgr linux-headers xdg-user-dirs xdg-utils
+# Actualizar base de datos
+pacman -Syy
+# Instalar paquetes base
+pacman -S efibootmgr linux-headers xdg-user-dirs xdg-utils xf86-video-intel
 
-# Instalar driver de vídeo
-pacman -S xf86-video-intel
-
-# Instalar el entorno de escriorio
-# Instalar los paquetes básicos
-sudo pacman -S gdm gnome-shell gnome-control-center gnome-terminal nautilus
-# Instalar el centro de software
-sudo pacman -S gnome-software gnome-software-packagekit-plugin
-# Instalar utilidades
-sudo pacman -S gnome-calculator eog gnome-screenshot gnome-system-monitor
-# Instalar paquetes opcionales
+# Instalar GNOME
+pacman -S gdm gnome-shell gnome-control-center gnome-terminal nautilus
+# Tienda
+pacman -S gnome-software gnome-software-packagekit-plugin
+# Utilidades
+sudo pacman -S gnome-text-editor gnome-calculator eog gnome-screenshot gnome-system-monitor
+# Opcional
 sudo pacman -S gnome-music gnome-calendar gnome-weather
-# Instalar fuentes
-sudo pacman -S otf-ipafont ttf-roboto ttf-fira-code ttc-iosevka
+# Fuentes
+sudo pacman -S ttf-fira-sans ttf-fira-code ttc-iosevka otf-ipafont
 
 # Habilitar los servicios
 sudo systemctl enable gdm
 sudo systemctl enable NetworkManager
-
